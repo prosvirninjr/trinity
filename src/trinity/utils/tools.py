2b672c29011d1cf2a312a3e1d15649dd2a12865c
Aleksandr Prosvirnin <prosvirnin.a@outlook.com>
2025-06-01 12:58:01 +0300

    patch

---
+++ src/trinity/utils/tools.py
@@ -0,0 +1,64 @@
+import hashlib
+import re
+from functools import cache
+
+
+class TextTools:
+    """Утилиты для работы со строками."""
+
+    @staticmethod
+    def to_clean(string: str) -> str:
+        """
+        Очищает строку от управляющих символов и лишних пробелов.
+
+        Args:
+            string (str): Исходная строка.
+
+        Returns:
+            str: Очищённая строка.
+        """
+        c_str = re.sub(r'\n', ' ', string)
+        c_str = re.sub(r'[\x00-\x09\x0B-\x1F\x7F-\x9F]', '', c_str)
+        return re.sub(r'\s+', ' ', c_str).strip()
+
+    @staticmethod
+    def to_trunc(string: str, max_length: int = 10) -> str:
+        """
+        Обрезает строку до заданной длины, добавляя '...' в конец. Итоговая длина строки учитывает '...'.
+
+        Args:
+            string (str): Исходная строка.
+            max_length (int): Максимальная длина результата.
+
+        Returns:
+            str: Обрезанная строка.
+        """
+        return string[: max(0, max_length - 3)] + '...' if len(string) > max_length else string
+
+    @staticmethod
+    def is_empty(string: str) -> bool:
+        """
+        Проверяет, является ли строка пустой или содержит только специальные метки.
+
+        Args:
+            string (str): Исходная строка.
+
+        Returns:
+            bool: True, если строка пустая или содержит только пробелы, 'None', '-', 'n/a'.
+        """
+        c_str = TextTools.to_clean(string)
+        return not c_str or c_str.lower() in ('none', '-', 'n/a')
+
+    @staticmethod
+    @cache
+    def get_hash(string: str) -> str:
+        """
+        Вычисляет SHA-256 хэш от строки.
+
+        Args:
+            string (str): Исходная строка.
+
+        Returns:
+            str: SHA-256 хэш строки.
+        """
+        return hashlib.sha256(string.encode('utf-8')).hexdigest()
---
+++ tests/utils/test_tools.py
@@ -0,0 +1,46 @@
+import pytest
+
+from trinity.utils.tools import TextTools
+
+
+class TestTextTools:
+    @pytest.mark.parametrize(
+        'string, expected',
+        [
+            ('Apple', 'Apple'),
+            ('  Apple  ', 'Apple'),
+            ('\nApple\n', 'Apple'),
+            ('\t\n\r Apple \t\n\r', 'Apple'),
+        ],
+    )
+    def test_to_clean(self, string: str, expected: str):
+        assert TextTools.to_clean(string) == expected
+
+    @pytest.mark.parametrize(
+        'string, max_length, expected',
+        [
+            ('Hello', 10, 'Hello'),
+            ('Hello, world!', 10, 'Hello, ...'),
+        ],
+    )
+    def test_to_trunc(self, string: str, max_length: int, expected: str):
+        assert TextTools.to_trunc(string, max_length) == expected
+
+    @pytest.mark.parametrize(
+        'string, expected',
+        [
+            ('-', True),
+            ('', True),
+            ('\n', True),
+            ('\t', True),
+            (' ', True),
+            ('n/a', True),
+            ('none', True),
+        ],
+    )
+    def test_is_empty(self, string: str, expected: bool):
+        assert TextTools.is_empty(string) == expected
+
+    @pytest.mark.skip(reason='Тестирование метода излишне, так как hashlib - встроенная библиотека.')
+    def test_get_hash(self):
+        pass
